7045a9831b4992e5aae3fe4058c1af15e0087a3b
Victor Alonso <vacontrerasmeza@gmail.com>
2018-09-05 02:05:04 -0500

    Project working and ready for deploy

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import * as firebase from 'firebase';
+
+//Substitution of home page for the SigninPage
+import { SigninPage } from '../pages/signin/signin';
+
+const config = {
+  apiKey: 'YOUR_APIKEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  databaseURL: 'YOUR_DATABASE_URL',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+};
+
+@Component({
+  templateUrl: 'app.html'
+})
+
+export class MyApp {
+  rootPage:any = SigninPage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+    firebase.initializeApp(config);
+  }
+}
+
